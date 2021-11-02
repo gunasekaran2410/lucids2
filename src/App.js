6eab744fd880333fa1cbd1a1362266ada2c0ea8f
gunasekaran2410 <61088656+gunasekaran2410@users.noreply.github.com>
2021-11-02 12:05:39 +0530

    logo update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react"
-import logo from "./logo.svg"
 import "./App.css"
 import { Row, Col, Container, Image} from 'react-bootstrap';
 import logo from '../src/assets/img/logo.png'
