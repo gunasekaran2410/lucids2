2ff927ff92c88f87b956cccc177e7e487c8759e7
gunasekaran2410 <61088656+gunasekaran2410@users.noreply.github.com>
2021-11-02 12:04:25 +0530

    app screen design

--- src/App.js
+++ src/App.js
@@ -1,47 +1,79 @@
 import React, { Component } from "react"
 import logo from "./logo.svg"
 import "./App.css"
-
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
+import { Row, Col, Container, Image} from 'react-bootstrap';
+import logo from '../src/assets/img/logo.png'
+import twitter from '../src/assets/img/twitter.png'
+import instagram from '../src/assets/img/instagram.png'
+import pinterest from '../src/assets/img/pinterest.png'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './Home'
+import Faq from './Faq';
+// import { BrowserRouter } from 'react-router-dom';
+import RoadMap from './Road';
+import Lunch from './Lunch'
+import pupa from '../src/assets/img/Pupa Labs Logo.png'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+        <Router>
+       <Container>
+          <Row>
+          <Col className='list' md={2} sm={0} xs={0}>
+             
+    </Col>
+    <Col className='list text-center' md={2} sm={6} xs={12}>
+            
+            <Link to="/RoadMap">ROAD MAP</Link>
+    </Col>
+   
+    <Col className='list' md={2} sm={6} xs={12}>
+            <div className='text-center'>
+            <Link to="/">
+              <Image className='logo'  src={logo} alt='Lucids Logo.png' /></Link>
+  </div>
+            </Col>
+            <Col className='list text-center' md={2} sm={6} xs={12}>
+             <Link to="/Faq">FAQs</Link>
+            </Col>
+            <Col className='list' md={3} sm={6} xs={12}>
+              <ul className='list-inline'>
+                <li className='list-inline-item'>
+               <Image src={twitter} alt='twitter' />
+                </li>
+                <li className='list-inline-item'>
+               <Image src={instagram} alt='instagram' />
+                </li>
+                <li className='list-inline-item'>
+               <Image src={pinterest} alt='pinterest' />
+                </li>
+             </ul>
+    </Col>
+  </Row>
+        </Container>
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/RoadMap" element={<RoadMap />} /> 
+        <Route path="/Faq" element={<Faq />} /> 
+        <Route path="/Lunch" element={<Lunch />} /> 
+      </Routes>
+      <Container>
+        <Row>
+       
+          <Col className='footer second_footer' sm={12} md={12} xs={12}>
+          <a href="#" > Conceptualised and executed by
+            </a><br></br>
+            <Image src={pupa} /><br></br>
+            <a  href="#"> Lucids.io 2021
+</a>
+          </Col>
+        
+        </Row>
+      </Container>
+
+   </Router>
       </div>
     )
   }
